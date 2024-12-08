f5d1b6fae36902c991350a4ea067a621b9af6ac5
dlsxody1 <dlsxody1@naver.com>
2024-12-08 23:25:00 +0900

    feat : cors 해결

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // CORS 설정
+  app.enableCors({
+    origin: true, // 모든 도메인 허용. 특정 도메인만 허용하려면 'http://example.com' 같이 문자열이나 배열로 지정
+    credentials: true, // 자격증명(쿠키 등) 허용
+    methods: ['GET'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
